58aeb17c5d288c202ae3fe7fa7270e849663d9ec
copaiement <cole.paiement@gmail.com>
2023-11-10 11:24:36 -0800

    Work on front page styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,10 @@ import "../styles/hero.css"
 export function Hero() {
   return (
     <div className="hero">
-      <div className="hero-header">RDSH World</div>
+      <div className="hero-bg"></div>
+      <div className="hero-header">
+        <img src="./src/assets/RDSHWorld.png"/>
+      </div>
       <p className="hero-text">
         We make the best Rock Climbing Clothing available today.
         Made in the USA and carefully crafted for performance
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ export function Navbar({ cart, items }) {
           <img className="navbar-logo" src="./src/assets/RDSH-Logo.avif" alt="" />
         </Link>
         <div className="navbar-links">
-          <Link className="navbar-link" to={`catalog`}>Catalog</Link>
+          <Link className="navbar-link text-link" to={`catalog`}>Catalog</Link>
         </div>
         <div className="navbar-right">
           <svg onClick={toggleSearch} className="icon navbar-link" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>magnify</title><path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" /></svg>
